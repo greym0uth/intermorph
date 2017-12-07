1645adcf60e8b7e99397a473b2fed4e3a3b4f849
Jaden Giordano <thejadenjack@gmail.com>
2017-12-07 16:03:02 -0500

    Adding linting

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    extends: 'eslint:recommended',
+	parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    }
+};
