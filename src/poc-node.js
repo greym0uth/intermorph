742981e20df1cdba0e94bdac8b0b9886065d878a
Jaden Giordano <thejadenjack@gmail.com>
2017-12-12 10:12:20 -0500

    Peer addition and broadcasting

--- src/poc-node.js
+++ src/poc-node.js
@@ -1,6 +1,7 @@
 /**
  * Proof-of-Concept for nodes and how they connect and work. P2P-Node will be used in after POC is completed.
  */
+const { info } = require('winston');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,13 +15,15 @@ app.use(bodyParser.json());
 let peers = [];
 let chain = new Chain();
 
-function replaceChain(newChain) { // eslint-disable-line no-unused-vars
-	if (newChain.isValid() && newChain.size() > chain.size())
-		chain = newChain;
-}
-
 app.get('/chain', (req, res) => {
-	res.send(JSON.stringify(chain));
+	res.json(chain);
+});
+
+app.post('/chain', (req, res) => {
+	let newChain = req.body.data || '';
+
+	if (newChain !== '' && newChain.isValid() && newChain.size() > chain.size())
+		chain = newChain;
 });
 
 app.post('/block', (req, res) => {
@@ -31,10 +34,10 @@ app.post('/block', (req, res) => {
 });
 
 app.get('/peers', (req, res) => {
-	res.send(JSON.stringify(peers));
+	res.json(peers);
 });
 
-app.post('/peers', (req, res) => {
+app.post('/peers/new', (req, res) => {
 	peers.push(req.body.data);
 
 	res.status(200).send();
@@ -42,5 +45,16 @@ app.post('/peers', (req, res) => {
 
 module.exports = {
 	app,
+	getChain: () => {
+		return chain;
+	},
+	broadcast: () => {
+		for (let i = 0; i < peers.length; i++) {
+			let peer = peers[i];
 
+			axios.post(`${peer}/chain`, chain).catch(err => {
+				info(`Unable to broadcast to peer ${peer}: ${JSON.stringify(err)}`);
+			});
+		}
+	}
 };
